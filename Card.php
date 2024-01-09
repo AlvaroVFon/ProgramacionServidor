fe131af7ec19c58e0a9eff0b6f09a4ee2afe4efd
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-01-09 09:02:07 +0100

    tarea evaluable enero

---
+++ Card.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+class Carta{
+
+  //propiedades
+
+private String $tipo;
+private int $puntos;
+
+private function __construct(String $tipo, int $puntos){
+  $this->tipo = $tipo ;
+  $this->puntos = $puntos;
+}
+
+//getters/setters
+
+function obternerTipo(){
+  return $this->tipo;
+}
+
+function obtenerPuntos(){
+  return $this->puntos;
+}
+
+function setTipo(String $tipo){
+  $this->tipo = $tipo;
+}
+
+function setPuntos(int $puntos){
+  $this->puntos = $puntos;
+}
+}
---
+++ enTipoCarta.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+enum enTipoCarta{
+  case Borde;
+  case Color;
+  case Efecto;
+  case Mensasje;
+  case Marco;
+}
